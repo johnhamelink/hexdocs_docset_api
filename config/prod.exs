747be237f8efdc7a8389c1482980cbef62fc86ba
John Hamelink <john@johnhamelink.com>
2016-10-03 05:20:29 +0100

    Various deployment tweaks
    - Removed crap from prod.exs
    - Added environment variable detection in prod
    - Added buildpack config to describe environment variables to expose

--- config/prod.exs
+++ config/prod.exs
@@ -13,49 +13,12 @@ use Mix.Config
 # which you typically run after static files are built.
 config :docset_api, DocsetApi.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "example.com", port: 80],
+  url: [host: {:system, "DOMAIN_NAME"}, port: 80],
   cache_static_manifest: "priv/static/manifest.json"
 
 # Do not print debug messages in production
 config :logger, level: :info
 
-# ## SSL Support
-#
-# To get SSL working, you will need to add the `https` key
-# to the previous section and set your `:url` port to 443:
-#
-#     config :docset_api, DocsetApi.Endpoint,
-#       ...
-#       url: [host: "example.com", port: 443],
-#       https: [port: 443,
-#               keyfile: System.get_env("SOME_APP_SSL_KEY_PATH"),
-#               certfile: System.get_env("SOME_APP_SSL_CERT_PATH")]
-#
-# Where those two env variables return an absolute path to
-# the key and cert in disk or a relative path inside priv,
-# for example "priv/ssl/server.key".
-#
-# We also recommend setting `force_ssl`, ensuring no data is
-# ever sent via http, always redirecting to https:
-#
-#     config :docset_api, DocsetApi.Endpoint,
-#       force_ssl: [hsts: true]
-#
-# Check `Plug.SSL` for all available options in `force_ssl`.
+config :docset_api, DocsetApi.Endpoint,
+  secret_key_base: {:system, "SECRET_KEY_BASE"}
 
-# ## Using releases
-#
-# If you are doing OTP releases, you need to instruct Phoenix
-# to start the server for all endpoints:
-#
-#     config :phoenix, :serve_endpoints, true
-#
-# Alternatively, you can configure exactly which server to
-# start per endpoint:
-#
-#     config :docset_api, DocsetApi.Endpoint, server: true
-#
-
-# Finally import the config/prod.secret.exs
-# which should be versioned separately.
-import_config "prod.secret.exs"
