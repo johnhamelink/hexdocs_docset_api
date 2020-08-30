06cbfc775742d81c387569943dd963eed641d97a
Mayel <git@mayel.space>
2020-08-30 21:15:26 +0200

    upgrade deps + cleanup

--- lib/models/release.ex
+++ lib/models/release.ex
@@ -1,12 +1,11 @@
 defmodule DocsetApi.Release do
-
   defstruct name: nil,
             version: nil,
             url: nil,
             docs_url: nil,
+            docs_html_url: nil,
             has_docs: nil,
             destination: nil,
             inserted_at: nil,
             updated_at: nil
-
 end
--- mix.exs
+++ mix.exs
@@ -2,46 +2,61 @@ defmodule DocsetApi.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :docset_api,
-     version: "0.0.1",
-     elixir: "~> 1.2",
-     elixirc_paths: elixirc_paths(Mix.env),
-     compilers: [:phoenix, :gettext] ++ Mix.compilers,
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     aliases: aliases(),
-     deps: deps()]
+    [
+      app: :docset_api,
+      version: "0.0.1",
+      elixir: "~> 1.2",
+      elixirc_paths: elixirc_paths(Mix.env()),
+      compilers: [:phoenix, :gettext] ++ Mix.compilers(),
+      build_embedded: Mix.env() == :prod,
+      start_permanent: Mix.env() == :prod,
+      aliases: aliases(),
+      deps: deps()
+    ]
   end
 
   # Configuration for the OTP application.
   #
   # Type `mix help compile.app` for more information.
   def application do
-    [mod: {DocsetApi, []},
-     applications: [:phoenix, :phoenix_pubsub, :phoenix_html, :cowboy, :logger, :gettext,
-                    :httpoison, :poison, :sqlitex, :floki]]
+    [
+      mod: {DocsetApi, []},
+      applications: [
+        :phoenix,
+        :phoenix_pubsub,
+        :phoenix_html,
+        :cowboy,
+        :logger,
+        :gettext,
+        :httpoison,
+        :poison,
+        :sqlitex,
+        :floki
+      ]
+    ]
   end
 
   # Specifies which paths to compile per environment.
   defp elixirc_paths(:test), do: ["lib", "web", "test/support"]
-  defp elixirc_paths(_),     do: ["lib", "web"]
+  defp elixirc_paths(_), do: ["lib", "web"]
 
   # Specifies your project dependencies.
   #
   # Type `mix help deps` for examples and options.
   defp deps do
-    [{:phoenix, "~> 1.2.1"},
-     {:phoenix_pubsub, "~> 1.0"},
-     {:phoenix_html, "~> 2.6"},
-     {:phoenix_live_reload, "~> 1.0", only: :dev},
-     {:gettext, "~> 0.11"},
-     {:cowboy, "~> 1.0"},
-     {:httpoison, "~> 0.9"},
-     {:poison, "~> 2.2"},
-     {:sqlitex, "~> 1.0"},
-     {:decimal, "1.2.0", override: true},
-     {:floki, "~> 0.10"}
-   ]
+    [
+      {:phoenix, "~> 1.2.1"},
+      {:phoenix_pubsub, "~> 1.0"},
+      {:phoenix_html, "~> 2.6"},
+      {:phoenix_live_reload, "~> 1.0", only: :dev},
+      {:gettext, "~> 0.11"},
+      {:plug_cowboy, "~> 1.0"},
+      {:httpoison, "~> 0.9"},
+      {:poison, "~> 2.2"},
+      {:sqlitex, "~> 1.0"},
+      {:decimal, "1.2.0", override: true},
+      {:floki, "~> 0.10"}
+    ]
   end
 
   # Aliases are shortcuts or tasks specific to the current project.
@@ -51,8 +66,11 @@ defmodule DocsetApi.Mixfile do
   #
   # See the documentation for `Mix` for more info on aliases.
   defp aliases do
-    ["ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
-     "ecto.reset": ["ecto.drop", "ecto.setup"],
-     "test": ["ecto.create --quiet", "ecto.migrate", "test"]]
+    [
+      "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
+      "ecto.reset": ["ecto.drop", "ecto.setup"],
+      test: ["ecto.create --quiet", "ecto.migrate", "test"],
+      start: ["phoenix.server"]
+    ]
   end
 end
