88d22f52f72ba1c6a8287e3dca0de4dbd5015174
John Hamelink <john@johnhamelink.com>
2020-09-02 21:43:42 +0100

    Cleaned up various warnings

--- lib/docset_api/endpoint.ex
+++ lib/docset_api/endpoint.ex
@@ -1,7 +1,9 @@
 defmodule DocsetApi.Endpoint do
   use Phoenix.Endpoint, otp_app: :docset_api
 
-  socket "/socket", DocsetApi.UserSocket
+  socket "/socket", DocsetApi.UserSocket,
+    websocket: true,
+    longpoll: false
 
   # Serve at "/" the static files from "priv/static" directory.
   #
