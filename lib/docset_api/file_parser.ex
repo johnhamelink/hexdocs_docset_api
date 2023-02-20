73b6f5648c1841731de09a127fb5074604eca459
Awea <aweaoftheworld@gmail.com>
2023-02-20 01:09:52 +0100

    Separate exceptions, pages and protocols from modules

--- lib/docset_api/file_parser.ex
+++ lib/docset_api/file_parser.ex
@@ -2,59 +2,83 @@ defmodule DocsetApi.FileParser do
   require Logger
 
   @spec parse_zeal_navigation(Floki.html_tree(), binary(), fun()) :: Floki.html_tree()
-  def parse_zeal_navigation(html, relative_path, callback) when is_function(callback) do
-    module = parse_module(html)
+  def parse_zeal_navigation(html_tree, relative_path, callback) when is_function(callback) do
+    module = parse_module(html_tree)
 
-    if module != "" do
-      callback.(module, "Module", "#{relative_path}#content")
+    body_classes =
+      Floki.attribute(html_tree, "body", "class")
+      |> Enum.join(" ")
 
-      for function <- parse_functions(html) do
-        callback.("#{module}.#{function}", "Function", "#{relative_path}##{function}")
-      end
+    is_guide =
+      String.contains?(body_classes, "page-extra") or
+        String.contains?(body_classes, "page-cheatmd")
 
-      for macro <- parse_macros(html) do
-        callback.("#{module}.#{macro}", "Macro", "#{relative_path}##{macro}")
-      end
+    is_exception = String.contains?(body_classes, "page-exception")
+    is_protocol = String.contains?(body_classes, "page-protocol")
 
-      for "c:" <> callback_id <- parse_callbacks(html) do
-        callback.("#{module}.#{callback_id}", "Callback", "#{relative_path}#c:#{callback_id}")
-      end
+    cond do
+      module != "" and not is_guide and not is_exception and not is_protocol ->
+        callback.(module, "Module", "#{relative_path}#content")
 
-      for "t:" <> type <- parse_types(html) do
-        callback.("#{module}.#{type}", "Type", "#{relative_path}#t:#{type}")
-      end
+        for function <- parse_functions(html_tree) do
+          callback.("#{module}.#{function}", "Function", "#{relative_path}##{function}")
+        end
+
+        for macro <- parse_macros(html_tree) do
+          callback.("#{module}.#{macro}", "Macro", "#{relative_path}##{macro}")
+        end
+
+        for "c:" <> callback_id <- parse_callbacks(html_tree) do
+          callback.("#{module}.#{callback_id}", "Callback", "#{relative_path}#c:#{callback_id}")
+        end
+
+        for "t:" <> type <- parse_types(html_tree) do
+          callback.("#{module}.#{type}", "Type", "#{relative_path}#t:#{type}")
+        end
+
+      is_protocol ->
+        callback.(module, "Protocol", "#{relative_path}#content")
+
+      is_exception ->
+        callback.(module, "Exception", "#{relative_path}#content")
+
+      is_guide ->
+        callback.(module, "Guide", "#{relative_path}#content")
+
+      true ->
+        :ok
     end
 
-    html
+    html_tree
   end
 
-  defp parse_module(html) do
-    html
+  defp parse_module(html_tree) do
+    html_tree
     |> Floki.find("#content>h1>span")
     |> Floki.text()
   end
 
-  defp parse_functions(html) do
-    html
+  defp parse_functions(html_tree) do
+    html_tree
     |> Floki.find("#functions .detail")
     |> Floki.attribute("id")
   end
 
-  defp parse_macros(html) do
-    html
+  defp parse_macros(html_tree) do
+    html_tree
     |> Floki.find("#macros .detail")
     |> Floki.attribute("id")
   end
 
-  defp parse_callbacks(html) do
-    html
+  defp parse_callbacks(html_tree) do
+    html_tree
     |> Floki.find(".detail")
     |> Floki.attribute("id")
     |> Enum.filter(&String.starts_with?(&1, "c:"))
   end
 
-  defp parse_types(html) do
-    html
+  defp parse_types(html_tree) do
+    html_tree
     |> Floki.find(".detail")
     |> Floki.attribute("id")
     |> Enum.filter(&String.starts_with?(&1, "t:"))
