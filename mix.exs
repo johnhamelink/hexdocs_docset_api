330eeec5f57faf0ea1e5ea63bb21850df2705d2c
John Hamelink <me@johnhame.link>
2024-11-05 00:45:17 +0000

    chore(Deps): Bump dependencies

--- mix.exs
+++ mix.exs
@@ -24,14 +24,14 @@ defmodule DocsetApi.Mixfile do
 
   defp deps do
     [
-      {:phoenix, "~> 1.5"},
-      {:phoenix_html, "~> 2.14"},
-      {:plug_cowboy, "~> 2.3"},
-      {:httpoison, "~> 1.7"},
-      {:poison, "~> 2.2 or ~> 3.0 or ~> 4.0"},
+      {:phoenix, "~> 1.7"},
+      {:phoenix_html, "~> 4.1"},
+      {:plug_cowboy, "~> 2.7"},
+      {:httpoison, "~> 2.2"},
+      {:poison, "~> 2.2 or ~> 3.0 or ~> 4.0 or ~> 5.0 or ~> 6.0"},
       {:sqlitex, "~> 1.7"},
-      {:esqlite, "~> 0.7.0", override: true},
-      {:floki, "~> 0.28"}
+      {:esqlite, "~> 0.8", override: true},
+      {:floki, "~> 0.36"}
     ]
   end
 end
