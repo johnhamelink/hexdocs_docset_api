d1a17cb4e719ece73c2380f56e4898c3a0601ee6
John Hamelink <me@johnhame.link>
2024-11-05 01:14:15 +0000

    chore(Flake): Added a simple nix flake

---
+++ flake.nix
@@ -0,0 +1,46 @@
+{
+  description = "Hexdocs Docset API - nix flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs =
+    inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+
+        "aarch64-darwin"
+        "x86_64-darwin"
+      ];
+
+      perSystem =
+        { pkgs, ... }:
+        {
+          devShells.default = pkgs.mkShell {
+            packages = with pkgs; [
+              # For Nix
+              mix2nix
+              nixd
+              nixfmt-rfc-style
+
+              # For Elixir
+              elixir
+              elixir-ls
+              fswatch # Used to re-run tests
+
+              # For Database
+              sqlite
+            ];
+          };
+        };
+    };
+}
